6304654326060b0c4e6b9eb4ec289c32497a71c8
Alexander Sheboltaev <a.sheboltaev@gmail.com>
2016-02-04 19:32:43 +0600

    slightly simplified List12_0

--- src/teaching/list/List12_0.java
+++ src/teaching/list/List12_0.java
@@ -2,22 +2,19 @@ package teaching.list;
 
 import teaching.list.utils.ArrayUtils;
 
-/**
- * Created by Женя on 29.01.2016.
- */
+
 public class List12_0 {
     public static void main(String[] args) {
         int[] a = new int[]{1, 2, 3, 4, 5, 6};
-        int k = 2;
+        int k = 29;
         ArrayUtils.printArray(rotate(a, k));
     }
 
     private static int[] rotate(int[] a, int k) {
         int[] result = new int[a.length];
-        for (int i = 0; i < a.length; i++) {
-            if (i < a.length - k) result[i] = a[i + k];
-            else result[i] = a[i - a.length + k];
-        }
+        for (int i = 0; i < a.length; i++)
+            result[i] = a[(i + k) % a.length];
         return result;
     }
 }
+
